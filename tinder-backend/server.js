4bbaa69c71e34278e44598df6b6eea958d2bc8e9
Rajat Saxena <rajatsaxena2504@gmail.com>
2021-05-10 15:24:47 +0530

    initial commit

---
+++ tinder-backend/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
+
+//App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url =
+  "mongodb+srv://admin:GfKs7TS7kxW8ndni@cluster0.c0jh7.mongodb.net/tinderdb?retryWrites=true&w=majority";
+
+//MiddleWare
+app.use(express.json());
+app.use(Cors());
+
+//DB Config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+//API endpoints
+app.get("/", (req, res) => res.status(200).send("HELLO CLEVER PROGRAMMERS!!"));
+
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/tinder/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+//Listner
+app.listen(port, () => console.log(`listening on the port : ${port}`));
